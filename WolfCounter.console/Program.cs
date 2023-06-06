85746312f7b1e909ab230862ed95e6f4ed7d183f
ik3 <aybikeboran7140@gmail.com>
2023-06-06 20:18:05 +0300

    Initial Commit

---
+++ WolfCounter.console/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inputFilePath = "Data/input.txt";
+        string outputFilePath = "Data/output.txt";
+        Console.WriteLine("Please enter the number of wolves you want to input:");
+        int numberOfWolves = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter the wolf IDs separated by spaces:");
+        List<int> wolfList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+        if (wolfList.Count < 5 || wolfList.Count > 2 * Math.Pow(10, 5))
+        {
+            Console.WriteLine("Invalid input. The number of wolfs must be between 5 and 200,000.");
+            wolfList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        }
+
+        while (wolfList.Count != numberOfWolves)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number of wolves.");
+            wolfList = Console.ReadLine().Split().Select(int.Parse).ToList();
+        }
+
+        foreach (int wolf in wolfList)
+        {
+            if (wolf < 1 || wolf > 5)
+            {
+                Console.WriteLine("Invalid input. Wolfs must have IDs between 1 and 5.");
+                wolfList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            }
+        }
+        int result = Result.UniqueWolfs(wolfList);
+        Console.WriteLine(result);
+    }
+}
---
+++ WolfCounter.console/Result.cs
@@ -0,0 +1,32 @@
+﻿public class Result
+{
+    public static int UniqueWolfs(List<int> wolfList)
+    {
+        Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+        foreach (int wolf in wolfList)
+        {
+            if (countMap.ContainsKey(wolf))
+                countMap[wolf]++;
+            else
+                countMap[wolf] = 1;
+        }
+
+        int maxCount = 0;
+        int minID = int.MaxValue;
+
+        foreach (KeyValuePair<int, int> pair in countMap)
+        {
+            int wolfID = pair.Key;
+            int count = pair.Value;
+
+            if (count > maxCount || (count == maxCount && wolfID < minID))
+            {
+                maxCount = count;
+                minID = wolfID;
+            }
+        }
+
+        return minID;
+    }
+}
